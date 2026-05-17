e8f7284141143f2252aced5f8e517d816368a8c0
WillSSSSuperP <3144714145@qq.com>
2026-05-18 02:21:55 +0800

    Delete src/main directory

--- src/main/java/com/end_crown/item/EndCrownTotemItem.java
+++
@@ -1,9 +0,0 @@
-package com.end_crown.item;
-
-import net.minecraft.world.item.Item;
-
-public class EndCrownTotemItem extends Item {
-    public EndCrownTotemItem() {
-        super(new Item.Properties().stacksTo(1));
-    }
-}
